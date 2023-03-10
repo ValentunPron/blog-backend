bbf45fa0232cf29e89fd36ba65617f3f3225e39b
Valentyn <valentunpron@gmail.com>
2023-03-10 15:11:38 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
-import cors from 'cors';
 
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
 import { UserController, PostController } from './controllers/index.js';
 import { handleValidationErrors, checkAuth } from './utils/index.js';
-
-mongoose.set("strictQuery", false);
 
 mongoose
 	.connect(process.env.MONGO_URL)
@@ -30,11 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json()); // Перетворює req в формат json
-app.use(cors());
 app.use('/uploads', express.static('uploads'));
-app.use((req, res) => {
-	console.log(res);
-})
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
@@ -47,18 +40,16 @@ app.post('/upload', upload.single('image'), (req, res) => {
 	});
 });
 
-app.get('/tags', PostController.getLastTags);
-
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 
 app.listen(process.env.PORT || 4444, (err) => {
 	if (err) {
 		return console.log(err);
 	}
-	
+
 	console.log('Server OK');
 });
