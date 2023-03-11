a2edc74826b63c0c22035df9b7b41e3e9b83898c
Valentyn <valentunpron@gmail.com>
2023-03-11 10:07:34 +0200

    test

--- index.js
+++ index.js
@@ -33,8 +33,12 @@ app.use(express.json()); // Перетворює req в формат json]
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "GET, POST, PUT, DELETE, OPTIONS"
+	);
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	next();
 })
 
