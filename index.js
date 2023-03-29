f6cf8ee070c3838b6db5276d9b9992f8bc54f18a
Valentyn <valentunpron@gmail.com>
2023-03-29 17:04:49 +0300

    Виправлення шляху

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 mongoose.set("strictQuery", false);
 //process.env.MONGO_URL
+//'mongodb+srv://admin:admin123@cluster0.ovflhbn.mongodb.net/blog?retryWrites=true&w=majority'
 mongoose
-	.connect('mongodb+srv://admin:admin123@cluster0.ovflhbn.mongodb.net/blog?retryWrites=true&w=majority')
+	.connect(process.env.MONGO_URL)
 	.then(() => console.log('DB ok'))
 	.catch((err) => console.log('DB error', err))
 
